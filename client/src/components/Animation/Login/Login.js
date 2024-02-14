27943e293873a292417de0ca060d2f2aea955bd3
MohdShahzil29 <Mohdshahzil59@gmail.com>
2024-02-14 15:31:27 +0530

    complete code

---
+++ client/src/components/Animation/Login/Login.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Login.css";
+
+const LoginAnimation = () => {
+  return (
+    <div>
+      <div className="flower">
+        <div className="f-wrapper">
+          <div className="flower__line" />
+          <div className="f">
+            <div className="flower__leaf flower__leaf--1" />
+            <div className="flower__leaf flower__leaf--2" />
+            <div className="flower__leaf flower__leaf--3" />
+            <div className="flower__leaf flower__leaf--4" />
+            <div className="flower__leaf flower__leaf--5" />
+            <div className="flower__leaf flower__leaf--6" />
+            <div className="flower__leaf flower__leaf--7" />
+            <div className="flower__leaf flower__leaf--8 flower__fall-down--yellow" />
+          </div>
+        </div>
+        <div className="f-wrapper f-wrapper--2">
+          <div className="flower__line" />
+          <div className="f">
+            <div className="flower__leaf flower__leaf--1" />
+            <div className="flower__leaf flower__leaf--2" />
+            <div className="flower__leaf flower__leaf--3" />
+            <div className="flower__leaf flower__leaf--4" />
+            <div className="flower__leaf flower__leaf--5" />
+            <div className="flower__leaf flower__leaf--6" />
+            <div className="flower__leaf flower__leaf--7" />
+            <div className="flower__leaf flower__leaf--8 flower__fall-down--pink" />
+          </div>
+        </div>
+        <div className="f-wrapper f-wrapper--3">
+          <div className="flower__line" />
+          <div className="f">
+            <div className="flower__leaf flower__leaf--1" />
+            <div className="flower__leaf flower__leaf--2" />
+            <div className="flower__leaf flower__leaf--3" />
+            <div className="flower__leaf flower__leaf--4" />
+            <div className="flower__leaf flower__leaf--5" />
+            <div className="flower__leaf flower__leaf--6" />
+            <div className="flower__leaf flower__leaf--7" />
+            <div className="flower__leaf flower__leaf--8 flower__fall-down--purple" />
+          </div>
+        </div>
+        <div className="flower__glass" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginAnimation;
